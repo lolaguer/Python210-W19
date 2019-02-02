3355e3e075a7f55bf950622096989bdc775e2b08
Lola Guerrero <mdguermor@gmail.com>
2019-02-01 16:06:38 -0800

    finish mailroom

--- students/lolaguerrero/session03/mailroom.py
+++ students/lolaguerrero/session03/mailroom.py
@@ -6,14 +6,12 @@
 # RRoot, 01/29/2019, Created file
 # ----------------------------------#
 
-
-###### Not finished yet!! #########
-
 import sys
+from prettytable import PrettyTable # I found this library to print nicely the donors summary
 
 # Data #
 # Donors table
-header = "Name, Donation" # Not use but helps to understand the data in our table
+header = "Name, Donation" # Not use but it helps to understand what the data means in our table
 p1 = "Bob Smith", [25, 100, 50]
 p2 = "Sue Jones", [750],
 p3 = "Frankie Addams", [10, 10, 10]
@@ -33,9 +31,7 @@ prompt = "\n".join(("Please choose from below options:",
 
 # Preprocessing #
 def donor_names(donors):
-
     """ Return the complete list of donors """
-
     d_names = []
     for row in donors:
         name = row[0]
@@ -44,9 +40,7 @@ def donor_names(donors):
 
 
 def donation_amount(name):
-
     """ Return a donor donation amount """
-
     donor_amount = int(input("How much money is the donor given?: "))
     for row in donors_table:
         if name == row[0]:
@@ -55,14 +49,13 @@ def donation_amount(name):
 
 
 def select_a_donor():
-
     """ Return a donor Full Name """
-
     donor_name = input("Please type the donor full name: ").title()
 
     if donor_name.lower() == 'list':
-        print ('Here is the list of the donor names:\n')
-        print (donor_names(donors_table), '\n')
+        print ('** Here is the list of the donor names:')
+        print (donor_names(donors_table))
+        print('\n')
     else:
         if donor_name not in donor_names(donors_table):
             new_donor = (donor_name, [])
@@ -78,9 +71,42 @@ def send_tank_you():
         return prompt
     else:
         amount = donation_amount(name)
+        print("\n")
         print (f"Dear {name}," + "\n" + f"We really appreciate your support donating to us the amount of ${amount}." + "\n" + "Thank you!!")
         print ("\n")
     return prompt
+
+
+def sort_amt(donor):
+    return int(donor[1].replace('$', '').replace('.00', ''))
+
+
+def create_report():
+    summary_donnors_table = []
+    for i in range(len(donors_table)):
+        donors_name = donors_table[i][0]
+        total_donated = sum(donors_table[i][1])
+        number_donations = len(donors_table[i][1])
+        average_donation = total_donated / number_donations
+        # Some formatting in the values
+        total_donated = '$%.2f' % total_donated
+        average_donation = '$%.2f' % average_donation
+        donor_sum = (donors_name, total_donated, number_donations, average_donation)
+        summary_donnors_table.append(donor_sum)
+
+    sorted_table = sorted(summary_donnors_table, key=sort_amt, reverse=True)
+
+    t = PrettyTable(["Donor Name", "Total Given", "Num Gifts", "Average Gift"])
+
+    for row in sorted_table:
+        t.add_row(list(row))
+
+    t.align["Donor Name"] = "l"
+    t.align["Total Given"] = "r"
+    t.align["Num Gifts"] = "r"
+    t.align["Average Gift"] = "r"
+
+    print (t)
 
 
 def exit_program():
@@ -94,7 +120,7 @@ def main():
         if response == "1":
             send_tank_you()
         elif response == "2":
-            print ('For now only 1 is ready!')
+            create_report()
         elif response == "3":
             exit_program()
         else:
