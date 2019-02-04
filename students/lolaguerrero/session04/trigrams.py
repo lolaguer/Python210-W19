8368cc7faa3cc415c05823632a6eb2207070610e
Lola Guerrero <mdguermor@gmail.com>
2019-02-03 18:43:25 -0800

    trigrams v2

--- students/lolaguerrero/session04/trigrams.py
+++ students/lolaguerrero/session04/trigrams.py
@@ -6,49 +6,69 @@
 # RRoot, 02/03/2019, Created file
 # ----------------------------------#
 
-
-words = 'I wish I may I wish I might'.split()
-
-def find_grams(grams):
-    trigram = []
-    grams = grams.split()
-    for i in range(len(words)):
-        for j in range(len(grams)-1):
-            if (grams[j] == words[i]) and (grams[j+1] == words[i+1]):
-                trigram.append(words[i+2])
-    return trigram
+import sys
+import random
+import string
 
 
-# Test
-assert find_grams("I wish") == ["I", "I"]
-assert find_grams("wish I") == ["may", "might"]
-assert find_grams("may I") == ["wish"]
-assert find_grams("I may") == ["I"]
 
-trigrams = {}
-def build_trigrams(grams):
-    #trigrams = {}
-    grams = grams.split()
-    for i in range(len(words)):
-        for j in range(len(grams)-1):
-            if (grams[j] == words[i]) and (grams[j+1] == words[i+1]):
-                k = tuple(grams)
-                if k in trigrams:
-                    trigrams[k] += [words[i+2]]
-                else:
-                    trigrams[k] = [words[i+2]]
+
+def read_in_data(file):
+    r_file = open(file, 'r')
+    text = []
+    for line in r_file:
+        line = line.replace('\n', '')
+        text.append(line)
+    return ' '.join(text)
+
+
+
+def make_words(text):
+    translator = str.maketrans(string.punctuation, ' ' * len(string.punctuation))
+    return text.translate(translator).split()
+
+
+
+def build_trigrams(words):
+    """
+    build up the trigrams dict from the list of words
+
+    returns a dict with:
+       keys: word pairs
+       values: list of followers
+    """
+    trigrams = {}
+
+    for i in range(len(words) - 2):
+        pair = words[i:i + 2]
+        follower = words[i + 2]
+        key = tuple(pair)
+        trigrams.setdefault(key, []).append(follower)
+
     return trigrams
 
 
-# Test
-print (build_trigrams("I wish"))
-print (build_trigrams("wish I"))
-print (build_trigrams("may I"))
-print (build_trigrams("I may"))
 
-assert trigrams == {("I", "wish"): ["I", "I"],
-                    ("wish", "I"): ["may", "might"],
-                    ("may", "I"): ["wish"],
-                    ("I", "may"): ["I"],
-                    }
+def build_text():
+
+    trigrams = build_trigrams(words)
+    word_pair = list(random.choice(list(trigrams.keys())))
+    print (" ".join(word_pair))
+
+
+
+if __name__ == "__main__":
+    # get the filename from the command line
+    try:
+        filename = sys.argv[1]
+    except IndexError:
+        print("You must pass in a filename")
+        sys.exit(1)
+
+    in_data = read_in_data(filename)
+    words = make_words(in_data)
+    word_pairs = build_trigram(words)
+    new_text = build_text(word_pairs)
+
+    print(new_text)
 
