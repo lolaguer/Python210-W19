e3d8a4473ea16ca211d605b9661f623244028aa2
Lola Guerrero <mdguermor@gmail.com>
2019-02-04 17:44:15 -0800

    trigrams v2

--- students/lolaguerrero/session04/trigrams.py
+++ students/lolaguerrero/session04/trigrams.py
@@ -14,22 +14,25 @@ import string
 
 
 def read_in_data(file):
+
     r_file = open(file, 'r')
     text = []
     for line in r_file:
-        line = line.replace('\n', '')
+        if line.startswith("**"):
+            line = line.replace('\n', '')
         text.append(line)
     return ' '.join(text)
 
 
 
 def make_words(text):
-    translator = str.maketrans(string.punctuation, ' ' * len(string.punctuation))
+
+    translator = str.maketrans('!"#$%&\'()*+-/:;<=>?@[\\]^_`{|}~', ' ' * len(string.punctuation))
     return text.translate(translator).split()
 
 
 
-def build_trigrams(words):
+def build_trigrams(text):
     """
     build up the trigrams dict from the list of words
 
@@ -38,6 +41,8 @@ def build_trigrams(words):
        values: list of followers
     """
     trigrams = {}
+
+    words = text.split()
 
     for i in range(len(words) - 2):
         pair = words[i:i + 2]
@@ -51,9 +56,27 @@ def build_trigrams(words):
 
 def build_text():
 
-    trigrams = build_trigrams(words)
-    word_pair = list(random.choice(list(trigrams.keys())))
-    print (" ".join(word_pair))
+    in_data = read_in_data("sherlock_small.txt")
+
+
+    trigrams = build_trigrams(in_data)
+
+    # Generating the first sentence:
+    word_pair = random.choice(list(trigrams.keys()))
+    new_word = random.choice(trigrams[word_pair])
+
+    # First sentence
+    text = " ".join(word_pair + (new_word,))
+
+    for _ in range(len(in_data)):
+        word_pair = tuple(text.split()[-2:])
+        new_word = random.choice(trigrams[word_pair])
+        list_of_words = word_pair + (new_word,)
+        new_sentence = " ".join(list_of_words)
+        text += new_sentence
+
+    return text
+
 
 
 
