25b1a966a2ad1b1fea32f83340e615490974a139
Lola Guerrero <mdguermor@gmail.com>
2019-02-19 20:31:50 -0800

    adding test_mailroom4 v2

--- students/lolaguerrero/session06/test_mailroom4.py
+++ students/lolaguerrero/session06/test_mailroom4.py
@@ -8,18 +8,21 @@
 
 import pytest
 from mailroom_4 import send_thank_you, create_report, send_letters
+import prettytable
+import os
 
 
-print (send_thank_you("Frank", 80000))
 
 def test_send_thank_you():
     expected = "Dear Frank," + "\n" + "We really appreciate your support donating to us the amount of $800000." + "\n" + "Thank you!!"
     assert send_thank_you("Frank", 800000) == expected
 
-#
-# #def test_create_report():
-#     assert 1 == 1
-#
-#
-# #def test_send_letters():
-#     assert 1 == 1
+
+def test_create_report():
+    assert type(create_report()) is prettytable.PrettyTable
+
+
+def test_send_letters():
+    send_letters()
+    path = "/Users/i22041/Documents/Python_Certification_UW/PYTHON210/Python210-W19/students/lolaguerrero/session06/thank_you_letters"
+    assert len(os.listdir(path)) != 0
