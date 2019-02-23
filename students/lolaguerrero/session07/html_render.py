50464adbbd7a5ef98a0c51adf766aaa7f1fcb7ab
Lola Guerrero <mdguermor@gmail.com>
2019-02-22 19:21:41 -0800

    adding step2

--- students/lolaguerrero/session07/html_render.py
+++ students/lolaguerrero/session07/html_render.py
@@ -19,3 +19,29 @@ class Element(object):
     def render(self, out_file):
         tag_name = {'tn' : self.tag}
         out_file.write("<{}> \n".format(self.tag) + self.content + " </{}>".format(self.tag))
+
+
+
+class Html(Element):
+
+    tag = "html"
+
+    def __init__(self, content=None):
+        self.content = content
+
+
+class Body(Element):
+
+    tag = "body"
+
+    def __init__(self, content=None):
+        self.content = content
+
+
+class Paragraph(Element):
+
+    tag = "p"
+
+    def __init__(self, content=None):
+        self.content = content
+
