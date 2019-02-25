179be7227cc339e71e3c08adb74858ad3cbf3750
Lola Guerrero <mdguermor@gmail.com>
2019-02-24 16:43:51 -0800

    passed pytest step1

--- students/lolaguerrero/session07/html_render.py
+++ students/lolaguerrero/session07/html_render.py
@@ -10,38 +10,11 @@ class Element(object):
 
     tag = "html"
 
-    def __init__(self, content=None):
+    def __init__(self, content=''):
         self.content = content
 
     def append(self, new_content):
         self.content = self.content +'.\n' + new_content + '.\n'
 
     def render(self, out_file):
-        tag_name = {'tn' : self.tag}
         out_file.write("<{}> \n".format(self.tag) + self.content + " </{}>".format(self.tag))
-
-
-
-class Html(Element):
-
-    tag = "html"
-
-    def __init__(self, content=None):
-        self.content = content
-
-
-class Body(Element):
-
-    tag = "body"
-
-    def __init__(self, content=None):
-        self.content = content
-
-
-class Paragraph(Element):
-
-    tag = "p"
-
-    def __init__(self, content=None):
-        self.content = content
-
